b1ada20b37f518b4a4ebe398ee8a756458b99bca
saikumar <142603396+saikumar-sanda@users.noreply.github.com>
2026-06-22 13:36:43 +0530

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/singlyLinkedList/submission-0.py
@@ -0,0 +1,63 @@
+class listNode:
+    def __init__(self,val,new_node = None):
+
+        self.val = val
+        self.next = new_node
+
+class LinkedList:
+    
+    def __init__(self):
+        
+        self.head = listNode(-1)
+        self.tail = self.head
+
+    
+    def get(self, index: int) -> int:
+
+        i = 0
+        curr = self.head.next
+        while curr:
+            if i == index and curr:
+                return curr.val
+            i += 1
+            curr = curr.next
+        return -1
+        
+    def insertHead(self, val: int) -> None:
+
+        new_node = listNode(val)
+        new_node.next = self.head.next
+        self.head.next = new_node
+
+        if not new_node.next :   
+                self.tail = new_node
+            
+    def insertTail(self, val: int) -> None:
+
+        self.tail.next = listNode(val)
+        self.tail = self.tail.next        
+
+    def remove(self, index: int) -> bool:
+        i = 0 
+        curr = self.head
+
+        while i < index and curr :
+            i += 1
+            curr = curr.next
+
+        if curr and curr.next:
+            if curr.next == self.tail:
+                self.tail = curr
+
+            curr.next = curr.next.next
+            return True
+        return False
+
+    def getValues(self) -> List[int]:
+        curr = self.head.next
+        res = []
+        while curr :
+            res.append(curr.val)
+            curr = curr.next
+        return res 
+        
